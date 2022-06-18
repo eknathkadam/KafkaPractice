d1e55bbc9ebf0a3b1e15812e801c220f3f437c45
eknathkadam <eknathkadam@gmail.com>
2022-06-18 15:33:46 +0530

    Initial checking

---
+++ basics/src/main/java/com/webrich/ProducerDemo.java
@@ -0,0 +1,33 @@
+package com.webrich;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+    public static void main(String[] args) {
+        log.info("Hello world!");
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("ORDER_PROCESSING_COMPLETED","Hi from Java class");
+        producer.send(producerRecord);
+        producer.flush();
+        producer.close();
+
+    }
+
+
+}
